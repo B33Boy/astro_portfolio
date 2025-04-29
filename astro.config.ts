379a10c974f1894ba29b768c3101d03f0de2b1e2
B33Boy <abhip202@gmail.com>
2025-04-29 19:52:29 -0400

    add older posts

--- astro.config.ts
+++ astro.config.ts
@@ -1,50 +1,56 @@
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
-import expressiveCode from 'astro-expressive-code';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import spectre from './package/src';
+import expressiveCode from "astro-expressive-code";
+import mdx from "@astrojs/mdx";
 
-import { spectreDark } from './src/ec-theme';
+import sitemap from "@astrojs/sitemap";
+import spectre from "./package/src";
+
+import { spectreDark } from "./src/ec-theme";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://abhipatel.netlify.app/',
-  output: 'static',
-  integrations: [
-    expressiveCode({
-      themes: [spectreDark],
-    }),
-    mdx(),
-    sitemap(),
-    spectre({
-      name: 'Abhi Patel',
-      openGraph: {
-        home: {
-          title: 'Abhi Patel',
-          description: 'Portfolio Site + Blog'
-        },
-        blog: {
-          title: 'Bits & Giggles',
-          description: 'Blog where I write about stuff'
-        },
-        projects: {
-          title: 'Projects'
-        }
-      },
-      // giscus: {
-      //   repository: 'louisescher/spectre',
-      //   repositoryId: 'R_kgDONjm3ig',
-      //   category: 'General',
-      //   categoryId: 'DIC_kwDONjm3is4ClmBF',
-      //   mapping: 'pathname',
-      //   strict: true,
-      //   reactionsEnabled: true,
-      //   emitMetadata: false,
-      //   lang: 'en',
-      // }
-    })
-  ],
-  adapter: netlify(),
+	site: "https://abhipatel.netlify.app/",
+	output: "static",
+	integrations: [
+		expressiveCode({
+			themes: [spectreDark],
+		}),
+		mdx(),
+
+		sitemap(),
+		spectre({
+			name: "Abhi Patel",
+			openGraph: {
+				home: {
+					title: "Abhi Patel",
+					description: "Portfolio Site + Blog",
+				},
+				blog: {
+					title: "Bits & Giggles",
+					description: "Blog where I write about stuff",
+				},
+				projects: {
+					title: "Projects",
+				},
+			},
+			// giscus: {
+			//   repository: 'louisescher/spectre',
+			//   repositoryId: 'R_kgDONjm3ig',
+			//   category: 'General',
+			//   categoryId: 'DIC_kwDONjm3is4ClmBF',
+			//   mapping: 'pathname',
+			//   strict: true,
+			//   reactionsEnabled: true,
+			//   emitMetadata: false,
+			//   lang: 'en',
+			// }
+		}),
+	],
+	markdown: {
+		// Disable Astro's default syntax highlighting
+		syntaxHighlight: false,
+	},
+	adapter: netlify(),
 });
